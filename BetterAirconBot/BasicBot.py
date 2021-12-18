991b8ec6684d400f15b3c68f04d2d96f0c1390db
= <=>
2021-12-18 16:48:46 +0800

    this is the first commit for the betterairconbot

---
+++ BetterAirconBot/BasicBot.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import os,time
+
+driver = webdriver.Chrome(executable_path="/Users/speedpowermac/Documents/projects/CODE_MAIN/chromedriver")
+driver.get()
