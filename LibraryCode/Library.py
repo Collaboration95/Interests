366d3be38c9fb707268e07aae16b42c3369351a0
= <=>
2021-12-19 10:58:27 +0800

    FirstCommit
    First commit for library telegram bot code

---
+++ LibraryCode/Library.py
@@ -0,0 +1,23 @@
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# import os,time
+# from selenium.webdriver.support.select import Select as DropDown
+# # from selenium.webdriver.support.ui import Select as DropDown
+
+# driver= webdriver.Chrome(executable_path="/Users/speedpowermac/Documents/projects/CODE_MAIN/chromedriver")
+
+# driver.get("https://mylibrary.sutd.edu.sg/roombooking")
+# listofelements=driver.find_elements(By.ID,'edit-location')
+# for i in range(len(listofelements)):
+#     print(listofelements[i].text)
+# # element_dropdown = driver.find_element_by_id('edit-location')
+# # select =DropDown(element_dropdown)
+# # try:
+# #     # select.select_by_visible_text('SI L2-26')
+# #     select.options()
+# #     print(select.options)
+# # except:
+# #     print("The item does not exist")
+# time.sleep(5)
+# driver.close()omm
+
