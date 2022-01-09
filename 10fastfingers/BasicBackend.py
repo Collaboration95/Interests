d9ac5e2d0502ff3fe9b15bc6d9ca10aab58af69c
= <=>
2022-01-09 21:40:20 +0800

    Working Code
    Havent done any actual word time setting

--- 10fastfingers/BasicBackend.py
+++ 10fastfingers/BasicBackend.py
@@ -4,35 +4,52 @@ from selenium.webdriver.common.keys import Keys
 import time 
 
 # Variables
-
 ExecutablePath = "/Users/speedpowermac/Documents/projects/CODE_MAIN/chromedriver"
 CookieButtonId = "CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll"
 MainInputFieldID = "inputfield"
+TargetWebpage = "https://10fastfingers.com/typing-test/english"
+ClassNameForWords = "highlight"
 
 def Main()->None:
     AutoWriterCode()
 
 def AutoWriterCode():
+    # This is the function performs the essential 
     driver = webdriver.Chrome(executable_path=ExecutablePath)
-    driver.get("https://10fastfingers.com/typing-test/english")
+    driver.get(TargetWebpage)
     time.sleep(4)
     # Allowing Cookie Collection
     CookieButton  = driver.find_element(By.ID,CookieButtonId)
     CookieButton.click()
 
-
-    html = driver.page_source
-    with open ("k.txt","w") as f:
-        f.write(html)
-
     InputField = driver.find_element(By.ID,MainInputFieldID)
-    for i in range(282):
-        A = driver.find_element(By.CLASS_NAME,"highlight")
-        A = A.text.split()
-        for i in range(len(A)):
-            time.sleep(.1)
-            InputField.send_keys(A[i])
-        InputField.send_keys(Keys.SPACE)
+    # for i in range(282):
+    time.sleep(2)
+
+    while True:
+        try:
+            A = driver.find_element(By.CLASS_NAME,ClassNameForWords)
+            InputField.send_keys(A.text+Keys.SPACE)
+        except:
+            timer =  ((driver.find_element(By.ID,"timer")).text)[2:]
+            print("Going to sleep for {} seconds".format(timer))
+            time.sleep(int(timer))
+
+            wpm = driver.find_element(By.ID,"wpm")
+            print(wpm.text)
+            accuracy = driver.find_element(By.ID,"accuracy")
+            print(accuracy.text)
+            driver.close()
+            break
+            pass
+        # K = [x for x in A.text]
+        # A = A.text.split("")
+        # for j in K:
+        #     InputField.send_keys(j)
+        
+    
+
+        # InputField.send_keys(Keys.SPACE)
         
 
 
--- 10fastfingers/Readme.md
+++ 10fastfingers/Readme.md
@@ -1,5 +1,7 @@
 __TO do__
 This bot opens a www.10fastfingers.com/typing-test/english and completes a test 
+Write Project Header ie Home page thingy in github
+Write Documentation 
 
 __Features__
 Ability to regulate wpm
