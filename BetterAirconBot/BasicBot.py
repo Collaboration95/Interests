755a25c44b706832e5644d8be62b3ae570f2d6ee
= <=>
2022-01-11 20:00:13 +0800

    AirconBot works
    The aircon thingy works but i believe i am wasting time by doing this and not working on the library bot. This will the be last commit on this for a while.The project isnt perfect but i will have to update it when i start wasting time once class starts.

--- BetterAirconBot/BasicBot.py
+++ BetterAirconBot/BasicBot.py
@@ -1,18 +1,17 @@
 from selenium import webdriver
+import re,time , hashlib
 # from selenium.webdriver.android.webdriver import WebDriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import time 
+
 # Variables required
 UserName = "20000486"
 PassWord = "Aircon0469"
 LoginButton = "btnLogin"
 LoginIdName = "txtLoginId"
 PasswordName = "txtPassword"
-Options  = {1:"Login",2:"exit"}
+Options  = {1:"Login",2:"DummyFunction",3:"exit"}
 
 ExecutablePath = "/Users/speedpowermac/Documents/projects/CODE_MAIN/chromedriver"
-
 
 def ShowAndGetOptions(OptionsDict:dict):
     for key in OptionsDict:
@@ -27,17 +26,43 @@ def ShowAndGetOptions(OptionsDict:dict):
 def Routing_function(N:int)->None:
     # This function is to route the functions and the correct options 
     if N==1:
-        pass
         Login_Setup()
     elif N==2:
+        IamWastingTime()
+    elif N==3:
         exit("Thank You for using the bot ")
+
+
+
+def IamWastingTime():
+    # I am wasting time 
+    password = 0
+
+    while (password!=95141):
+        password = input("Please Input password :")
+    
+    
+    with open ("BetterAirconBot/Balance.txt","r") as f:
+        TempContent = f.readlines()
+    
+    TempContent = [x.strip("\n") for x in TempContent ]
+    Content = {}
+    FinalContent = []
+    for i in range(len(TempContent)):
+        TempContent1 =  TempContent[i].split(" ")
+        Content["UnixTime"] = TempContent1[0]
+        Content["NormalTime"] = TempContent1[1] 
+        Content["Balance"] = TempContent1[2]
+        Content["Hashed"] = TempContent1[3]
+        FinalContent.append(Content.copy())
+
 
 def Login_Setup():
 
 
     if(input("Press Enter if you want to use defualt")!=""):
-        UserName = input("Input UserName")
-        PassWord = input("Input Password")
+        UserName = input("Input UserName :")
+        PassWord = input("Input Password :")
     else:
         UserName = "20000486"
         PassWord = "Aircon0469"
@@ -62,28 +87,44 @@ def Login_Setup():
     driver.execute_script("window.open('about:blank','secondtab');")
     driver.switch_to.window("secondtab")
     driver.get("https://nus-utown.evs.com.sg/SUTDMain/viewMeterCreditServlet")
+    
+    # Getting XPATH for balance
     element = driver.find_element(By.XPATH,"//*[@id='frmViewMeterCredit']/table/tbody/tr[4]/td[3]/font")
     Balance = element.get_attribute('innerHTML')
-    IntBalance = float(Balance[18:])
-    print("The Remaining Balance is {}".format(IntBalance))
+
+    # Getting Last Recorded TimeStamp
+    element1 = driver.find_element(By.XPATH,"/html/body/table/tbody/tr[1]/td[2]/form/table/tbody/tr[9]/td[2]/font")
+    UnixTime = Epoch_Time(element1.text)
+
+    FloatBalance = float(Balance[18:])
+    print("The Remaining Balance is {}".format(FloatBalance))
+    NormalTime = re.sub(' ','|',element1.text)
+    
+    
+
+    WriteData(UnixTime,NormalTime,FloatBalance,Sha256Hashing(UserName)) #Calls a function to write info into a text file      
+    
     time.sleep(3)
     driver.close()
 
+def WriteData(UnixTime,NormalTime,Balance:float,HashedName):
+    # This function writes the current time and 
+    with open ("BetterAirconBot/Balance.txt","a") as f:
+        f.write("{} {} {} {}".format(UnixTime,NormalTime,Balance,HashedName))
+        f.write("\n")
 
-# def SecondPage(driver1):
-#     # This is the code that runs in the second page/thepage after the login page
-#     driver1.find_element(By.LINK_TEXT,"Web Calculator")
-#     driver1.switch_to.frame("leftFrame")
-#     driver1.click()    
-    
-#     pass
+def Sha256Hashing(CurrentUserName:str):
+    # This function is here because i just learnt Sha256 function 
+    return hashlib.sha256(CurrentUserName.encode()).hexdigest()
+
+def Epoch_Time(Time_String) ->str:
+    # Converts the given time in "dd.mm.yyy hh:mm:ss" into epoch time 
+    Converted_Time = time.mktime(time.strptime(Time_String, "%d/%m/%Y %H:%M:%S"))
+    return str(int(Converted_Time))
+
 
 def MainFunction():
     # This is the first function that is called when the program is executed
-    
-    
     Routing_function(ShowAndGetOptions(Options))
-
-    pass
 
 MainFunction()
